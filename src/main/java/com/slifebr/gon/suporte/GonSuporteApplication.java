0a555d28c79a9ff56e9591ceb4f84b0db81183a3
Sergio Lima <slifebr@hotmail.com>
2020-10-31 18:26:51 -0300

    initial

---
+++ src/main/java/com/slifebr/gon/suporte/GonSuporteApplication.java
@@ -0,0 +1,13 @@
+package com.slifebr.gon.suporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonSuporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonSuporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slifebr/gon/suporte/GonSuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slifebr.gon.suporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonSuporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
